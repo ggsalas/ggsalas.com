4cd42f9943c8798fe3c7bedba8fe6d385ea79280
Gabriel Salas <ggsalas@gmail.com>
2019-05-03 23:18:31 -0300

    Updates

---
+++ precache-manifest.82cfe8f3a6b89d6ca28a904bc27896e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5becf0a2b70cd9698005b69536676b8",
+    "url": "/ggsalas.com/index.html"
+  },
+  {
+    "revision": "9bb3b70278b3f3f721fd",
+    "url": "/ggsalas.com/static/css/main.6cce10d8.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/ggsalas.com/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "9bb3b70278b3f3f721fd",
+    "url": "/ggsalas.com/static/js/main.bbeed45a.chunk.js"
+  },
+  {
+    "revision": "2c2b2b342d59edc7e151",
+    "url": "/ggsalas.com/static/js/runtime~main.7e09be7b.js"
+  }
+]);
